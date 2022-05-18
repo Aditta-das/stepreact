6e2c3937e2281623e409e92663fb7679e7ceb3d9
Aditta Das Nishad <60479691+Aditta-das@users.noreply.github.com>
2022-05-18 13:35:31 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import QuestionForm from './Pages/question';
